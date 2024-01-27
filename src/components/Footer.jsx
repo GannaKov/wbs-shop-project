c37ad17ba68f89443388c28e0b3d6e342906897b
Ruben Otero <ruben87sp@gmail.com>
2024-01-27 13:14:00 +0100

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,10 @@
+import Footer1 from "./Footer1";
+import Footer2 from "./Footer2";
+
 const Footer = () => {
-  return <div>I am footer</div>;
+  return <div>
+    <Footer1 />
+    <Footer2 />
+  </div>;
 };
 export default Footer;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,10 @@
+import hero from "../assets/images/imagesRuben/hero.webp";
 const Hero = () => {
-  return <div>I am Hero !!!!!</div>;
+  return (
+  <div>
+    <img src={hero} alt="Saloon" />
+    </div>);
+  
 };
 
 export default Hero;
