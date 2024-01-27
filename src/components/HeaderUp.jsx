ef821691111ab1ab8f3d22fbdafd9c6db1bb193c
GannaKov <zlatta2000@gmail.com>
2024-01-27 13:06:59 +0100

    mobile menu

--- src/components/HeaderUp.jsx
+++ src/components/HeaderUp.jsx
@@ -1,15 +1,18 @@
 import styles from "../styles/HeaderUp.module.css";
+// import HeaderUpMobile from "./HeaderUpMobile";
 import HeaderLogo from "./HeaderLogo";
 import HeaderNav from "./HeaderNav";
 import Button from "./Button";
 
 const HeaderUp = () => {
   return (
-    <div className={styles.upHeaderContainer}>
-      <HeaderLogo />
-      <HeaderNav />
-      <Button />
-    </div>
+    <>
+      <div className={styles.upHeaderContainer}>
+        <HeaderLogo />
+        <HeaderNav />
+        <Button />
+      </div>
+    </>
   );
 };
 
