d8cfed530b16c5b875130c8c643b2921faf9a4c3
GannaKov <zlatta2000@gmail.com>
2024-01-27 15:46:21 +0100

    mobile menu styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,11 @@
 import styles from "../styles/Header.module.css";
-
 import HeaderBottom from "./HeaderBottom";
-
 import HeaderUp from "./HeaderUp";
 
 const Header = () => {
   return (
     <header className={styles.headerSection}>
       <HeaderUp />
-
       <HeaderBottom />
     </header>
   );
--- src/components/HeaderNav.jsx
+++ src/components/HeaderNav.jsx
@@ -2,35 +2,33 @@ import styles from "../styles/HeaderNav.module.css";
 
 const HeaderNav = () => {
   return (
-    <nav className={styles.desktopNav}>
-      <ul className={styles.listHeaderUpContainer}>
-        <li className={styles.itemHeaderUpContainer}>
-          <a className={styles.linkNavHeaderUpContainer} href="#">
-            HOME
-          </a>
-        </li>
-        <li className={styles.itemHeaderUpContainer}>
-          <a className={styles.linkNavHeaderUpContainer} href="#">
-            ABOUT
-          </a>
-        </li>
-        <li className={styles.itemHeaderUpContainer}>
-          <a className={styles.linkNavHeaderUpContainer} href="#">
-            SERVICES
-          </a>
-        </li>
-        <li className={styles.itemHeaderUpContainer}>
-          <a className={styles.linkNavHeaderUpContainer} href="#">
-            PRODUCTS
-          </a>
-        </li>
-        <li className={styles.itemHeaderUpContainer}>
-          <a className={styles.linkNavHeaderUpContainer} href="#">
-            CONTACT
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <ul className={styles.listHeaderUpContainer}>
+      <li className={styles.itemHeaderUpContainer}>
+        <a className={styles.linkNavHeaderUpContainer} href="">
+          HOME
+        </a>
+      </li>
+      <li className={styles.itemHeaderUpContainer}>
+        <a className={styles.linkNavHeaderUpContainer} href="#">
+          ABOUT
+        </a>
+      </li>
+      <li className={styles.itemHeaderUpContainer}>
+        <a className={styles.linkNavHeaderUpContainer} href="#">
+          SERVICES
+        </a>
+      </li>
+      <li className={styles.itemHeaderUpContainer}>
+        <a className={styles.linkNavHeaderUpContainer} href="#">
+          PRODUCTS
+        </a>
+      </li>
+      <li className={styles.itemHeaderUpContainer}>
+        <a className={styles.linkNavHeaderUpContainer} href="#">
+          CONTACT
+        </a>
+      </li>
+    </ul>
   );
 };
 
