73c13c275f6f0c2523b512affe34078228b53288
Ruben Otero <ruben87sp@gmail.com>
2024-01-27 14:27:15 +0100

    Third commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,8 @@
+import styles from "../styles/About.module.css";
+
 const About = () => {
   return (
-  <div>
+  <div className={styles.aboutContainer}>
     <h3>We have the freedom to choose, so we chose to be good.</h3>
     <p>At Mane Ethical Hairdressing we have exercised our power to choose, and in doing so we have created a space and a community of shared values. Being sustainable and ethical isn't always the easiest of paths, however we made a conscious effort to explore and research salon options that positively contribute to making a better world.
 We hope you enjoy what we have created.</p>
