e2e3388ed10b9f5107718bef311894074557d59e
Ruben Otero <ruben87sp@gmail.com>
2024-02-03 13:01:26 +0100

    Responsiveness

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
     <h2>We have the freedom to choose, so we chose to be good.</h2>
     <p>At Mane Ethical Hairdressing we have exercised our power to choose, and in doing so we have created a space and a community of shared values. Being sustainable and ethical isn't always the easiest of paths, however we made a conscious effort to explore and research salon options that positively contribute to making a better world.<br />
 We hope you enjoy what we have created.</p>
-    <a href="https://apps.kitomba.com/bookings/maneethical" target="_blank" rel="noreferrer"><div className={styles.appbtn}>book appointment now</div></a>
+    <a href="https://apps.kitomba.com/bookings/maneethical" target="_blank" rel="noreferrer"><div className={styles.appBtn}>book appointment now</div></a>
   </div>);
 };
 
--- src/components/Footer1.jsx
+++ src/components/Footer1.jsx
@@ -5,40 +5,42 @@ import instagram from "../assets/images/imagesRuben/instagram.webp";
 const Footer1 = () => {
     return (
         <div className={styles.footerContainer}>
-            <div>
+            <div className={styles.footerLogo}>
                 <img src={logoFooter} alt="" />
             </div>
-            <div className={styles.openingH}>
-                <div>Opening Hours (by appointment only)</div>
-                <div className={styles.daysW}>
-                    <div>
-                        <div id="day">Mon</div>
-                        <div id="day">Tue</div>
-                        <div id="day">Wed</div>
-                        <div id="day">Thurs</div>
-                        <div id="day">Fri</div>
-                        <div id="day">Sat</div>
-                        <div id="day">Sun</div>
-                    </div>
-                    <div className={styles.timesW}>
-                        <div id="time">9am-3pm</div>
-                        <div id="time">9am-3pm</div>
-                        <div id="time">9am-3pm</div>
-                        <div id="time">9am-7pm</div>
-                        <div id="time">9am-7pm</div>
-                        <div id="time">9am-3pm</div>
-                        <div id="time">closed</div>
+                <div className={styles.openingH}>
+                    <div>Opening Hours (by appointment only)</div>
+                    <div className={styles.daysW}>
+                        <div>
+                            <div id="day">Mon</div>
+                            <div id="day">Tue</div>
+                            <div id="day">Wed</div>
+                            <div id="day">Thurs</div>
+                            <div id="day">Fri</div>
+                            <div id="day">Sat</div>
+                            <div id="day">Sun</div>
+                        </div>
+                        <div className={styles.timesW}>
+                            <div id="time">9am-3pm</div>
+                            <div id="time">9am-3pm</div>
+                            <div id="time">9am-3pm</div>
+                            <div id="time">9am-7pm</div>
+                            <div id="time">9am-7pm</div>
+                            <div id="time">9am-3pm</div>
+                            <div id="time">closed</div>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className={styles.contact}>
-                <div className={styles.tlf}><a href="https://www.google.com/search?q=mane+ethical+hair&oq=mane+eth&aqs=chrome.0.69i59l2j69i57j0i22i30j0i10i22i30j0i22i30j0i10i22i30j69i60.5805j0j7&sourceid=chrome&ie=UTF-8" target="_blank" rel="noreferrer">(07) 5659 1905</a></div>
-                <div>6 / 5-7 Tallebudgera Creek Road, Burleigh, Queensland</div>
-                <div className={styles.socialMedia}>
-                    <a href="https://www.facebook.com/maneethicalhairdressing" target="_blank" rel="noreferrer"><img src={facebook} alt="facebookLogo" /></a>
-                    <a href="https://www.instagram.com/maneethicalhair/" target="_blank" rel="noreferrer"><img src={instagram} alt="instagramLogo" /></a>
+                <div className={styles.contact}>
+                    
+                    <div className={styles.contactTitle}>Contact details</div>
+                    <div className={styles.tlf}><a href="https://www.google.com/search?q=mane+ethical+hair&oq=mane+eth&aqs=chrome.0.69i59l2j69i57j0i22i30j0i10i22i30j0i22i30j0i10i22i30j69i60.5805j0j7&sourceid=chrome&ie=UTF-8" target="_blank" rel="noreferrer">(07) 5659 1905</a></div>
+                    <div>6 / 5-7 Tallebudgera Creek Road, <br />Burleigh, Queensland</div>
+                    <div className={styles.socialMedia}>
+                        <a href="https://www.facebook.com/maneethicalhairdressing" target="_blank" rel="noreferrer"><img src={facebook} alt="facebookLogo" /></a>
+                        <a href="https://www.instagram.com/maneethicalhair/" target="_blank" rel="noreferrer"><img src={instagram} alt="instagramLogo" /></a>
+                    </div>
                 </div>
-            </div>
         </div>);
   };
   export default Footer1;
